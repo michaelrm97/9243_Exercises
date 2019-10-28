7f58379d693bb708613bd141806430ea66586b93
Michael Manansala <michaelricardomanansala@gmail.com>
2019-10-28 14:59:59 +1100

    Got client server benchmark working

--- ring/benchmark_cs.erl
+++ ring/benchmark_cs.erl
@@ -1,5 +1,5 @@
 -module(benchmark_cs).
--export([benchmark/2, query_keys/1, client/1]).
+-export([benchmark/2, run_benchmark/2, query_keys/1, client/2]).
 
 fill_with_keys(0) ->
   ok;
@@ -13,27 +13,31 @@ query_keys(N) ->
   dbclient:lookup(N),
   query_keys(N - 1).
 
-client(N) ->
+client(N, P) ->
   query_keys(N),
-  benchmarker ! done.
+  P ! done.
 
-spawn_clients(_, 0) ->
+spawn_clients(_, 0, _) ->
   ok;
-spawn_clients(N, C) ->
-  spawn(?MODULE, client, [N]),
-  spawn_clients(N, C - 1).
+spawn_clients(N, C, P) ->
+  spawn(?MODULE, client, [N, P]),
+  spawn_clients(N, C - 1, P).
 
 wait_for_clients(0) ->
   ok;
 wait_for_clients(C) ->
   receive
-    {done} -> wait_for_clients(C - 1)
+    done -> 
+      wait_for_clients(C - 1)
   end.
 
-benchmark(N, C) ->
-  register(benchmarker, self()),
+run_benchmark(N, C) ->
   dbserv:start(),
   fill_with_keys(N),
-  spawn_clients(N, C),
+  spawn_clients(N, C, self()),
   wait_for_clients(C),
-  dbclient:stop().
+  dbclient:stop(),
+  io:format("Done!\n").
+
+benchmark(N, C) ->
+  spawn(?MODULE, run_benchmark, [N, C]).
